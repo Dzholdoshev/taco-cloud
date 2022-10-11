7da03990a18350d24414a4182cc9c862e177c1fa
marklen <marklen86@gmail.com>
2022-10-11 16:45:58 -0500

    Creating controller OrderController class, Creating Model TacoOrder. (&2 Developing wep applications)

--- src/main/java/com/sia/tacos/model/Taco.java
+++ src/main/java/com/sia/tacos/model/Taco.java
@@ -2,11 +2,25 @@ package com.sia.tacos.model;
 
 import lombok.Data;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
+    @Email
+    @NotNull
+    @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+  //  @Size(min=3, message="Name must be at least 3 characters long")
+
+
 
 }
